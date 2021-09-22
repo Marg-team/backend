6eeace73737f753fb43aed6906589e77f9f8631d
Angshuman Barpujari <angshuarin@gmail.com>
2021-09-22 19:41:03 +0530

    donation for ngo-volunteer extra api, status change added

--- controller/donationController.js
+++ controller/donationController.js
@@ -1,4 +1,5 @@
 const Donation = require('../models/donationModel');
+const UserModel = require('../models/userModel')
 
 exports.submitForm = async (req, res) => { 
     try{
@@ -32,12 +33,76 @@ exports.getAllForm = async (req, res) => {
     }
 }
 
+exports.getAllFormNGO = async (req, res) => { 
+    try{
+        const user = await UserModel.findOne({_id:req.user._id}).populate()
+        if(!user){
+            throw new Error("User is not found");
+        }
+        if(!user.ngoRef){
+            throw new Error("User is not an NGO");
+        }
+
+        const donations = await Donation.find({assignedTo: user.ngoRef}).populate()
+        res.status(200).json({
+            status: 'success',
+            donationForm: donations,
+        });
+    }catch (err) {
+        console.log(err)
+        res.status(400).json({
+            status: 'fail',
+            message: err,
+        });
+    }
+}
+
+exports.getAllFormVolunteer = async (req, res) => { 
+    try{
+        const donations = await Donation.find({$or:[{status: 0}, {status: null}]}).populate();
+        res.status(200).json({
+            status: 'success',
+            donationForm: donations,
+        });
+    }catch (err) {
+        res.status(400).json({
+            status: 'fail',
+            message: err,
+        });
+    }
+}
+
 exports.getForm = async (req, res) => {
     try{
         const donation = await Donation.findOne({
             _id: req.params.id,
         });
         res.status(200).json({
+            status: 'success',
+            donationForm: donation,
+        });
+    }catch (err) {
+        res.status(400).json({
+            status: 'fail',
+            message: err,
+        });
+    }
+}
+
+exports.statusChange = async (req, res) => {
+    try{
+        const status = req.params.status;
+
+        if(status===1){
+            const assignedTo = req.body.assignedTo;
+            if(!assignedTo){
+                new Error('Assigned to is not passed');
+            }
+            const donation = await Donation.updateOne({_id: req.params.id}, {status: 1, assignedTo: assignedTo});
+        }else{
+            const donation = await Donation.updateOne({_id: req.params.id}, {status: status, assignedTo: null});
+        }
+        res.status(201).json({
             status: 'success',
             donationForm: donation,
         });
--- routes/api/donationRoute.js
+++ routes/api/donationRoute.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const donationController = require('../../controller/donationController');
+const passport = require('passport')
 
 
 router.route('/').post(donationController.submitForm);
 router.route('/').get(donationController.getAllForm);
-router.route('/:id').get(donationController.getForm);
+router.route('/ngo').get(passport.authenticate('jwt', { session: false }), donationController.getAllFormNGO);
+router.route('/volunteer').get(passport.authenticate('jwt', { session: false }), donationController.getAllFormVolunteer);
+router.route('/:id').get(passport.authenticate('jwt', { session: false }), donationController.getForm);
+router.route('/:id/:status').post(passport.authenticate('jwt', { session: false }), donationController.statusChange)
 
 module.exports = router;
