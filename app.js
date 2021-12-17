40a5094c1b590a68ed7b07b380389656281116b3
Angshuman Barpujari <angshuarin@gmail.com>
2021-12-17 20:06:30 +0530

    view added to backend

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ const limiter = rateLimit({
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'views/build')));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
@@ -54,8 +55,8 @@ mongoose.connect(DB, {
   console.log('DB connection successful');
 });
 
-app.use('/', indexRouter);
 app.use('/api/', apiRouter); 
+app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+const path = require('path');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Ashroy Api' });
+
+router.get('/*', function(req, res, next) {
+  res.sendFile(path.join(__dirname, '../views/build', 'index.html'));
 });
 
 module.exports = router;
